5b6d7b2b6c8d5bd5f61d3b9511ca46bcfa9817eb
Nilaa Maharjan <59684223+MaNikhilHo@users.noreply.github.com>
2024-02-10 22:36:59 +0100

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -68,7 +68,7 @@ const CONFIG = {
   },
   resume: {
     fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      'public/Nilaa Maharjan-CV_1.pdf', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
     'PHP',
