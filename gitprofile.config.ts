8bb4f292589ca92e89b09ce89aab360e5ef7d9d7
Nilaa Maharjan <59684223+MaNikhilHo@users.noreply.github.com>
2024-02-18 13:00:29 +0100

    Update gitprofile.config.ts
    testing medium blog pull

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -130,8 +130,8 @@ const CONFIG = {
   ],
   // Display articles from your medium or dev account. (Optional)
   blog: {
-    source: 'dev', // medium | dev
-    username: 'manikhilho', // to hide blog section, keep it empty
+    source: 'medium', // medium | dev
+    username: '@realnikhiljyapu', // to hide blog section, keep it empty
     limit: 3, // How many articles to display. Max is 10.
   },
   googleAnalytics: {
